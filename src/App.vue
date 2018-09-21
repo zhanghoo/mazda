aaca6a2c5dd7096b6b470665d12b7c2756cfbea8
uncleLian <771674109@qq.com>
2018-09-21 10:25:57 +0800

    调整了chat适配和iframe适配

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
     <div id="app">
         <transition name="el-fade-in-linear" mode="out-in">
-            <router-view />
+            <router-view/>
         </transition>
+        <div class="iframe-button">金牌导购</div>
     </div>
 </template>
 
@@ -21,6 +22,23 @@ export default {
     a {
         text-decoration: none;
         outline: none;
+    }
+}
+@media screen and (max-width: 200px) {
+    #app {
+        flex-center();
+        width: 100%;
+        height: 100%;
+        background: #000;
+        *:not(.iframe-button) {
+            display: none;
+        }
+        .iframe-button {
+            color: #999;
+            font-size: 14px;
+            font-weight: 600;
+            padding: 5px 0;
+        }
     }
 }
 </style>
